bc6623a5ce6a30458f9bea42cb962eccc6147546
ci.internal.kevinlin.info <ci@kevinlin.info>
2019-03-13 01:39:46 +0000

    [automatic commit] origin/master:ed5967d98bdeb0762620a1b799aa115728f114d2

--- extensions-manager/dist/dist.js
+++ extensions-manager/dist/dist.js
@@ -618,54 +618,6 @@ module.exports = emptyFunction;
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _HttpManager = __webpack_require__(7);
-
-var _HttpManager2 = _interopRequireDefault(_HttpManager);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var RepoController = function () {
-  function RepoController(props) {
-    _classCallCheck(this, RepoController);
-
-    this.repo = props.repo;
-  }
-
-  _createClass(RepoController, [{
-    key: "getPackages",
-    value: function getPackages(callback) {
-      var _this = this;
-
-      _HttpManager2.default.get().getAbsolute(this.repo.url, {}, function (response) {
-        _this.response = response;
-        callback(response);
-      }, function (error) {
-        console.log("Error loading repo", error);
-        callback(null);
-      });
-    }
-  }]);
-
-  return RepoController;
-}();
-
-exports.default = RepoController;
-
-/***/ }),
-/* 5 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
 /*
 object-assign
 (c) Sindre Sorhus
@@ -759,7 +711,7 @@ module.exports = shouldUseNative() ? Object.assign : function (target, source) {
 
 
 /***/ }),
-/* 6 */
+/* 5 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -780,6 +732,54 @@ if (false) {
 }
 
 module.exports = emptyObject;
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _HttpManager = __webpack_require__(7);
+
+var _HttpManager2 = _interopRequireDefault(_HttpManager);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var RepoController = function () {
+  function RepoController(props) {
+    _classCallCheck(this, RepoController);
+
+    this.repo = props.repo;
+  }
+
+  _createClass(RepoController, [{
+    key: "getPackages",
+    value: function getPackages(callback) {
+      var _this = this;
+
+      _HttpManager2.default.get().getAbsolute(this.repo.url, {}, function (response) {
+        _this.response = response;
+        callback(response);
+      }, function (error) {
+        console.log("Error loading repo", error);
+        callback(null);
+      });
+    }
+  }]);
+
+  return RepoController;
+}();
+
+exports.default = RepoController;
 
 /***/ }),
 /* 7 */
@@ -845,7 +845,6 @@ var HttpManager = function () {
       }
 
       xmlhttp.open(verb, url, true);
-      xmlhttp.setRequestHeader('Content-type', 'application/json');
 
       if (verb == "post" || verb == "patch") {
         xmlhttp.send(JSON.stringify(params));
@@ -900,7 +899,7 @@ var _Repo = __webpack_require__(2);
 
 var _Repo2 = _interopRequireDefault(_Repo);
 
-var _RepoController = __webpack_require__(4);
+var _RepoController = __webpack_require__(6);
 
 var _RepoController2 = _interopRequireDefault(_RepoController);
 
@@ -1366,8 +1365,6 @@ var ManageInstalled = function (_React$Component) {
       return _react2.default.createElement(
         "div",
         { className: "sk-panel-section no-border" },
-        _react2.default.createElement("div", { className: "sk-panel-row" }),
-        _react2.default.createElement("div", { className: "sk-panel-row" }),
         _react2.default.createElement(
           "div",
           { className: "sk-panel-section-outer-title info sk-bold" },
@@ -1486,7 +1483,7 @@ if (!Array.prototype.includes) {
  * LICENSE file in the root directory of this source tree.
  */
 
-var m=__webpack_require__(5),n=__webpack_require__(6),p=__webpack_require__(3),q="function"===typeof Symbol&&Symbol["for"],r=q?Symbol["for"]("react.element"):60103,t=q?Symbol["for"]("react.call"):60104,u=q?Symbol["for"]("react.return"):60105,v=q?Symbol["for"]("react.portal"):60106,w=q?Symbol["for"]("react.fragment"):60107,x="function"===typeof Symbol&&Symbol.iterator;
+var m=__webpack_require__(4),n=__webpack_require__(5),p=__webpack_require__(3),q="function"===typeof Symbol&&Symbol["for"],r=q?Symbol["for"]("react.element"):60103,t=q?Symbol["for"]("react.call"):60104,u=q?Symbol["for"]("react.return"):60105,v=q?Symbol["for"]("react.portal"):60106,w=q?Symbol["for"]("react.fragment"):60107,x="function"===typeof Symbol&&Symbol.iterator;
 function y(a){for(var b=arguments.length-1,e="Minified React error #"+a+"; visit http://facebook.github.io/react/docs/error-decoder.html?invariant\x3d"+a,c=0;c<b;c++)e+="\x26args[]\x3d"+encodeURIComponent(arguments[c+1]);b=Error(e+" for the full message or use the non-minified dev environment for full errors and additional helpful warnings.");b.name="Invariant Violation";b.framesToPop=1;throw b;}
 var z={isMounted:function(){return!1},enqueueForceUpdate:function(){},enqueueReplaceState:function(){},enqueueSetState:function(){}};function A(a,b,e){this.props=a;this.context=b;this.refs=n;this.updater=e||z}A.prototype.isReactComponent={};A.prototype.setState=function(a,b){"object"!==typeof a&&"function"!==typeof a&&null!=a?y("85"):void 0;this.updater.enqueueSetState(this,a,b,"setState")};A.prototype.forceUpdate=function(a){this.updater.enqueueForceUpdate(this,a,"forceUpdate")};
 function B(a,b,e){this.props=a;this.context=b;this.refs=n;this.updater=e||z}function C(){}C.prototype=A.prototype;var D=B.prototype=new C;D.constructor=B;m(D,A.prototype);D.isPureReactComponent=!0;function E(a,b,e){this.props=a;this.context=b;this.refs=n;this.updater=e||z}var F=E.prototype=new C;F.constructor=E;m(F,A.prototype);F.unstable_isAsyncReactComponent=!0;F.render=function(){return this.props.children};var G={current:null},H=Object.prototype.hasOwnProperty,I={key:!0,ref:!0,__self:!0,__source:!0};
@@ -1562,7 +1559,7 @@ if (true) {
 /*
  Modernizr 3.0.0pre (Custom Build) | MIT
 */
-var aa=__webpack_require__(0),l=__webpack_require__(14),B=__webpack_require__(5),C=__webpack_require__(3),ba=__webpack_require__(15),da=__webpack_require__(16),ea=__webpack_require__(17),fa=__webpack_require__(18),ia=__webpack_require__(21),D=__webpack_require__(6);
+var aa=__webpack_require__(0),l=__webpack_require__(14),B=__webpack_require__(4),C=__webpack_require__(3),ba=__webpack_require__(15),da=__webpack_require__(16),ea=__webpack_require__(17),fa=__webpack_require__(18),ia=__webpack_require__(21),D=__webpack_require__(5);
 function E(a){for(var b=arguments.length-1,c="Minified React error #"+a+"; visit http://facebook.github.io/react/docs/error-decoder.html?invariant\x3d"+a,d=0;d<b;d++)c+="\x26args[]\x3d"+encodeURIComponent(arguments[d+1]);b=Error(c+" for the full message or use the non-minified dev environment for full errors and additional helpful warnings.");b.name="Invariant Violation";b.framesToPop=1;throw b;}aa?void 0:E("227");
 var oa={children:!0,dangerouslySetInnerHTML:!0,defaultValue:!0,defaultChecked:!0,innerHTML:!0,suppressContentEditableWarning:!0,suppressHydrationWarning:!0,style:!0};function pa(a,b){return(a&b)===b}
 var ta={MUST_USE_PROPERTY:1,HAS_BOOLEAN_VALUE:4,HAS_NUMERIC_VALUE:8,HAS_POSITIVE_NUMERIC_VALUE:24,HAS_OVERLOADED_BOOLEAN_VALUE:32,HAS_STRING_BOOLEAN_VALUE:64,injectDOMPropertyConfig:function(a){var b=ta,c=a.Properties||{},d=a.DOMAttributeNamespaces||{},e=a.DOMAttributeNames||{};a=a.DOMMutationMethods||{};for(var f in c){ua.hasOwnProperty(f)?E("48",f):void 0;var g=f.toLowerCase(),h=c[f];g={attributeName:g,attributeNamespace:null,propertyName:f,mutationMethod:null,mustUseProperty:pa(h,b.MUST_USE_PROPERTY),
@@ -2225,11 +2222,7 @@ var _BridgeManager = __webpack_require__(1);
 
 var _BridgeManager2 = _interopRequireDefault(_BridgeManager);
 
-var _InstallRepo = __webpack_require__(27);
-
-var _InstallRepo2 = _interopRequireDefault(_InstallRepo);
-
-var _Advanced = __webpack_require__(28);
+var _Advanced = __webpack_require__(27);
 
 var _Advanced2 = _interopRequireDefault(_Advanced);
 
@@ -2337,49 +2330,6 @@ var Home = function (_React$Component) {
         _react2.default.createElement(
           "div",
           { className: "sk-panel-content" },
-          this.state.ready && this.state.repos.length == 0 && _react2.default.createElement(_InstallRepo2.default, null),
-          (this.state.downloading || this.state.validUntil) && _react2.default.createElement(
-            "div",
-            { className: "sk-panel-section no-bottom-pad" },
-            this.state.downloading && _react2.default.createElement(
-              "div",
-              null,
-              _react2.default.createElement(
-                "div",
-                { className: "sk-panel-row justify-left sk-horizontal-group" },
-                _react2.default.createElement("div", { className: "sk-spinner info small" }),
-                _react2.default.createElement(
-                  "p",
-                  null,
-                  "Refreshing packages..."
-                )
-              ),
-              _react2.default.createElement("div", { className: "sk-panel-row" })
-            ),
-            !this.state.downloading && this.state.validUntil && _react2.default.createElement(
-              "div",
-              null,
-              _react2.default.createElement(
-                "div",
-                { className: "sk-panel-row justify-left sk-horizontal-group" },
-                _react2.default.createElement("div", { className: "sk-circle small " + (this.isExpired() ? "danger" : "success") }),
-                _react2.default.createElement(
-                  "p",
-                  null,
-                  "Your Extended benefits ",
-                  this.isExpired() ? "expired on" : "are valid until",
-                  " ",
-                  this.state.validUntil.toLocaleString()
-                ),
-                _react2.default.createElement(
-                  "a",
-                  { className: "info", onClick: this.refreshValidUntil },
-                  " Refresh "
-                )
-              ),
-              _react2.default.createElement("div", { className: "sk-panel-row" })
-            )
-          ),
           _react2.default.createElement(_ManageInstalled2.default, null),
           this.state.repos.map(function (repo, index) {
             return _react2.default.createElement(_RepoView2.default, { key: index, repo: repo, ref: function ref(_ref) {
@@ -2426,7 +2376,7 @@ var _Repo = __webpack_require__(2);
 
 var _Repo2 = _interopRequireDefault(_Repo);
 
-var _RepoController = __webpack_require__(4);
+var _RepoController = __webpack_require__(6);
 
 var _RepoController2 = _interopRequireDefault(_RepoController);
 
@@ -3320,166 +3270,6 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
 
 /***/ }),
 /* 27 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _react = __webpack_require__(0);
-
-var _react2 = _interopRequireDefault(_react);
-
-var _Repo = __webpack_require__(2);
-
-var _Repo2 = _interopRequireDefault(_Repo);
-
-var _RepoController = __webpack_require__(4);
-
-var _RepoController2 = _interopRequireDefault(_RepoController);
-
-var _BridgeManager = __webpack_require__(1);
-
-var _BridgeManager2 = _interopRequireDefault(_BridgeManager);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var InstallRepo = function (_React$Component) {
-  _inherits(InstallRepo, _React$Component);
-
-  function InstallRepo(props) {
-    _classCallCheck(this, InstallRepo);
-
-    var _this = _possibleConstructorReturn(this, (InstallRepo.__proto__ || Object.getPrototypeOf(InstallRepo)).call(this, props));
-
-    _this.handleKeyPress = function (e) {
-      if (e.key === 'Enter') {
-        _this.installProLink(_this.state.url);
-      }
-    };
-
-    _this.handleChange = function (event) {
-      _this.setState({ url: event.target.value });
-    };
-
-    _this.submitUrl = function () {
-      _this.installProLink(_this.state.url);
-    };
-
-    _this.state = { url: "" };
-    return _this;
-  }
-
-  _createClass(InstallRepo, [{
-    key: "installProLink",
-    value: function installProLink(url) {
-      var decoded;
-      try {
-        // base64 decode
-        decoded = atob(url);
-      } catch (e) {}
-
-      if (decoded) {
-        url = decoded;
-      }
-
-      _BridgeManager2.default.get().installRepoUrl(url);
-      this.setState({ url: "" });
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      return _react2.default.createElement(
-        "div",
-        { id: "install-repo", className: "sk-panel-section" },
-        _react2.default.createElement(
-          "div",
-          { className: "sk-panel-row centered" },
-          _react2.default.createElement(
-            "div",
-            { className: "sk-h2" },
-            _react2.default.createElement(
-              "strong",
-              null,
-              "Enter Your Extended Activation Code"
-            )
-          )
-        ),
-        _react2.default.createElement(
-          "div",
-          { className: "sk-notification contrast dashed one-line" },
-          _react2.default.createElement("input", {
-            className: "sk-input clear center-text",
-            placeholder: "Enter Extended Code",
-            type: "url",
-            value: this.state.url,
-            onKeyPress: this.handleKeyPress,
-            onChange: this.handleChange
-          })
-        ),
-        this.state.url && this.state.url.length > 0 && _react2.default.createElement(
-          "div",
-          { id: "submit-button", className: "sk-panel-row centered" },
-          _react2.default.createElement(
-            "a",
-            { onClick: this.submitUrl, className: "sk-button success big" },
-            _react2.default.createElement(
-              "div",
-              { className: "sk-label" },
-              "Submit Code"
-            )
-          )
-        ),
-        _react2.default.createElement(
-          "div",
-          { className: "sk-panel-row centered" },
-          _react2.default.createElement(
-            "div",
-            { className: "sk-h1 center-text" },
-            _react2.default.createElement(
-              "strong",
-              { className: "info" },
-              "Standard Notes Extended"
-            ),
-            " gives you access to powerful editors, extensions, tools, themes, and cloud backup options."
-          )
-        ),
-        _react2.default.createElement("div", { className: "sk-panel-row" }),
-        _react2.default.createElement(
-          "div",
-          { className: "sk-panel-row centered" },
-          _react2.default.createElement(
-            "a",
-            { href: "https://standardnotes.org/extensions", target: "_blank", className: "sk-button info featured" },
-            _react2.default.createElement(
-              "div",
-              { className: "sk-label" },
-              "Learn More"
-            )
-          )
-        )
-      );
-    }
-  }]);
-
-  return InstallRepo;
-}(_react2.default.Component);
-
-exports.default = InstallRepo;
-
-/***/ }),
-/* 28 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
